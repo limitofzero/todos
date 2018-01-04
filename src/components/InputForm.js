daaee5ffd3d7b6d54a5ecfaeb225125a44012998
Denis Makarov <limitofzero@gmail.com>
2018-01-04 23:19:33 +0300

    Добавлены обработчики нажатия клавиш и изменения вводимого значения

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "../less/input-form.less";
 
-export default class InputForm extends Component {
+class InputForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ""
+        }
+    }
+
+    onKeyPress(event) {
+        if(event.key === 'Enter') {
+            console.log('Enter pressed');
+        }
+    }
+
+    onValueChanged(event) {
+        this.setState({value: event.target.value});
+    }
 
     render() {
         return (
             <div className="input-form">
-                <input type="text" placeholder="Введите текст..."/>
+                <input type="text"
+                       placeholder="Введите текст..."
+                       value={this.state.value}
+                       onChange={this.onValueChanged.bind(this)}
+                       onKeyPress={this.onKeyPress.bind(this)}/>
             </div>
         );
     }
 }
+
+export default connect(null)(InputForm);
